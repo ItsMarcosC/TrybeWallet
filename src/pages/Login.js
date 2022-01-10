63636deaf3895a831eeb152e0b09e9eba36f4235
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-01-10 18:16:23 -0300

    Challenge 1 Done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import { setUser } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+      buttonDisableStatus: true,
+    };
+  }
+
+  handleValidation = () => {
+    const { email, password } = this.state;
+    const validRegex = /\S+@\S+\.\S+/;
+    const minimumPasswordChars = 6;
+
+    if (validRegex.test(email) && password.length >= minimumPasswordChars) {
+      this.setState({ buttonDisableStatus: false });
+    } else {
+      this.setState({ buttonDisableStatus: true });
+    }
+  }
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.handleValidation();
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, buttonDisableStatus, redirect } = this.state;
+    const { user } = this.props;
+
+    if (redirect) return <Redirect to="/carteira" />;
+
+    return (
+      <>
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="Email"
+          name="email"
+          onChange={ this.handleChange }
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Password"
+          name="password"
+          onChange={ this.handleChange }
+        />
+
+        <button
+          type="button"
+          onClick={ () => {
+            user(email);
+            this.setState({ redirect: true });
+          } }
+          disabled={ buttonDisableStatus }
+        >
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  user: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  user: (email) => dispatch(setUser(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
