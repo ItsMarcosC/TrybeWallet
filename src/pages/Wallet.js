c27918d19a662a7ed372534ca005f0d96f4f022e
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-01-10 19:05:07 -0300

    Challenge 3 Done

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet!</div>;
+    const displayEmail = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">
+          {displayEmail}
+        </p>
+
+        <p data-testid="total-field">
+          0
+        </p>
+
+        <p data-testid="header-currency-field">
+          BRL
+        </p>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  displayEmail: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
