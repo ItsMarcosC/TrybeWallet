d501d19b961e4d23d6ffa50f93fc4be66d734f8a
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-01-11 19:00:09 -0300

    Test Evaluator for Challenges 4,5,6 and 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Form from '../components/Form';
 import Table from '../components/Table';
@@ -16,4 +15,4 @@ class Wallet extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, null)(Wallet);
+export default Wallet;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,9 +1,24 @@
+import { ADD_VALUE, GET_CURRENCIES } from '../actions';
+
 const INITIAL_STATE = {
-  value: '',
+  currencies: [],
+  expenses: [],
+  totalValue: 0,
 };
 
 const walletReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case ADD_VALUE:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload],
+      totalValue: state.totalValue + (parseFloat(action.payload.value) * action.ask),
+    };
+  case GET_CURRENCIES:
+    return {
+      ...state,
+      currencies: action.payload,
+    };
   default:
     return state;
   }
