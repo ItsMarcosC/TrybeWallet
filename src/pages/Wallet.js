67ee334a4e13a90bebb769c0957e80a158a44ef0
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-01-11 18:43:17 -0300

    Fix Wallet Page, now it renders 3 components instead of having logic inside

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import Form from '../components/Form';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
-    const { displayEmail } = this.props;
     return (
-      <header>
-        <p data-testid="email-field">
-          {displayEmail}
-        </p>
-
-        <p data-testid="total-field">
-          0
-        </p>
-
-        <p data-testid="header-currency-field">
-          BRL
-        </p>
-      </header>
+      <>
+        <Header />
+        <Form />
+        <Table />
+      </>
     );
   }
 }
-
-const mapStateToProps = (state) => ({
-  displayEmail: state.user.email,
-});
-
-Wallet.propTypes = {
-  displayEmail: PropTypes.func.isRequired,
-};
 
 export default connect(mapStateToProps, null)(Wallet);
