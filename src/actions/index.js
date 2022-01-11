2e2799dbc929fe867b3949c32321883eae012f75
Marcos Carvalho <marcosfilhosp@hotmail.com>
2022-01-11 17:10:55 -0300

    Add all the actions needed for Challenge 4 and 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,32 @@
 export const SAVE_USER = 'SAVE_USER';
+export const FETCH_API = 'FETCH_API';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const ADD_VALUE = 'ADD_VALUE';
 
-export const setUser = (email) => ({
+export const setUser = (payload) => ({
   type: SAVE_USER,
-  email,
+  payload,
 });
+
+const fetchApi = () => ({
+  type: FETCH_API,
+});
+
+export const getCurrencies = (payload) => ({
+  type: GET_CURRENCIES,
+  payload,
+});
+
+// Nota pro futuro, 'ask' é o valor que vem da API.
+export const addValue = (payload, ask) => ({
+  type: ADD_VALUE,
+  payload,
+  ask,
+});
+
+export const xxx = () => (dispatch) => {
+  dispatch(fetchApi());
+  return fetch('https://economia.awesomeapi.com.br/json/all')
+    .then((response) => response.json())
+    .then((payload) => dispatch(getCurrencies(payload)));
+};
